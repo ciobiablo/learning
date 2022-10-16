1a023f822539d2335e1a85c70eb7933eadf2ffba
Ahmet Han Uçmaz <ahmethanucmaz@Ahmets-MacBook-Pro.local>
2022-10-16 18:01:12 +0300

    PHP :: Php file added

---
+++ PHP/UNIT1/sihirli-karakterler.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+        Sihirli Karakterler
+        \t
+        \n
+        \\
+ *      \$
+ *      \'
+ *      \"
+ */
+$test = "ahmet\\han   \\test\ ";
+//echo $test ;
+
+$ad = "ahmet";
+//echo"\$ad değişkeni $ad değerine eşittir.";
+
+echo 'ahmet dediki:\'ben korkam\'.. ';
