4180043f9136b8892a8fd3ab3c119da22010f25d
Ahmet Han Uçmaz <ahmethanucmaz@Ahmets-MacBook-Pro.local>
2022-11-02 15:15:50 +0300

    title name changed

--- cms/admin/view/static/header.php
+++ cms/admin/view/static/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>AhmetHan</title>
 
     <!--styles-->
     <link rel="stylesheet" href="<?= admin_public_url('styles/main.css?v=' . time()) ?>">
